403faae274727aa61783610f92b032586d855658
Thibault Bruggeman <bruggeman.dev@gmail.com>
2022-01-17 12:57:37 +0100

    Update output path

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,6 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "public/js/dist/"),
+    path: path.resolve(__dirname, "public/dist/"),
   },
 };
